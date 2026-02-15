de04e7bb0f9bc7bcfdd60415c378b3b585fafccd
Ricardo Sanchez <ricsanfre@gmail.com>
2026-02-15 20:22:14 +0100

    Remove obsolete ansible tasks and roles

--- ansible/roles/logging/external_node/templates/adjust_ts.lua
+++
@@ -1,7 +0,0 @@
-function local_timestamp_to_UTC(tag, timestamp, record)
-    local utcdate   = os.date("!*t", ts)
-    local localdate = os.date("*t", ts)
-    localdate.isdst = false -- this is the trick
-    utc_time_diff = os.difftime(os.time(localdate), os.time(utcdate))
-    return 1, timestamp - utc_time_diff, record
-end
